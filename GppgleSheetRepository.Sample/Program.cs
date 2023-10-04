ff4feaa21f98d70b0c9d1369796bc07f31bc8b9d
DolG <dolganov.g@gmail.com>
2023-10-04 07:29:00 +0500

    get products work

--- GppgleSheetRepository.Sample/Program.cs
+++ GppgleSheetRepository.Sample/Program.cs
@@ -22,36 +22,17 @@ builder.Services.AddSingleton<IRepository<Category>, Repository<Category>>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
+app.MapGet("/products", (IRepository<Product> repository) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return repository.Get();
 })
-.WithName("GetWeatherForecast")
+.WithName("Google sheet repository")
 .WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
